9cb958a24c858a7eec94e5312eca6ebbd3d1717d
LucasLombardo <lucasalombardo@gmail.com>
2019-03-12 19:36:56 -0400

    Add user info to quotes serializers
    Add user info to the quotes serializer including user id and username

--- app/serializers/quote_serializer.rb
+++ app/serializers/quote_serializer.rb
@@ -1,3 +1,9 @@
 class QuoteSerializer < ActiveModel::Serializer
-  attributes :id, :body, :attribution
+  attributes :id, :body, :attribution, :owner
+
+  # extract relevant owning user attributes
+  def owner
+    {owner_id: self.object.user.id, 
+     owner_account: self.object.user.email}
+  end 
 end
