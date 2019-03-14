59a2721d3ba0a7922a2def06514b58d059196d27
LucasLombardo <lucasalombardo@gmail.com>
2019-03-13 23:28:19 -0400

    Add rails_id alias to quote id
    Add rails_id alias to quote id to avoid id getting overwritten by
    graphQl server on frontend

--- app/serializers/quote_serializer.rb
+++ app/serializers/quote_serializer.rb
@@ -1,9 +1,14 @@
 class QuoteSerializer < ActiveModel::Serializer
-  attributes :id, :body, :attribution, :owner
+  attributes :id, :body, :attribution, :owner, :rails_id
 
   # extract relevant owning user attributes
   def owner
     {owner_id: self.object.user.id, 
      owner_account: self.object.user.email}
-  end 
+  end
+
+  # give id an alias so graphQl doesn't overwrite id
+  def rails_id
+    self.object.id
+  end
 end
