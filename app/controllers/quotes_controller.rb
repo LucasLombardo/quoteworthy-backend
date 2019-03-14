f304fc34bb3232c48e2be7e2ae1ccaef334dbf37
LucasLombardo <lucasalombardo@gmail.com>
2019-03-14 11:54:54 -0400

    Add webhooks for triggering front end builds
    Add webhooks on quote update, edit, and delete to trigger front end
    builds

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,10 +1,24 @@
+require "uri"
+require 'net/http'
+require 'net/https'
+
+# post request to trigger front end builds
+def post_netlify_webhook
+    uri = URI.parse("https://api.netlify.com/build_hooks/5c8a5ae08e0d9701e58d93ef")
+    uri.query = URI.encode_www_form({})
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    request = Net::HTTP::Post.new(uri.request_uri)
+    http.request(request).body
+end
+
 class QuotesController < OpenReadController
   before_action :set_quote, only: [:show, :update, :destroy]
 
   # GET /quotes
   def index
     @quotes = Quote.all
-
     render json: @quotes
   end
 
@@ -19,6 +33,8 @@ class QuotesController < OpenReadController
 
     if @quote.save
       render json: @quote, status: :created, location: @quote
+      # trigger front end build with new data
+      post_netlify_webhook if Rails.env.production?
     else
       render json: @quote.errors, status: :unprocessable_entity
     end
@@ -28,6 +44,8 @@ class QuotesController < OpenReadController
   def update
     if @quote.update(quote_params)
       render json: @quote
+      # trigger front end build with new data
+      post_netlify_webhook if Rails.env.production?
     else
       render json: @quote.errors, status: :unprocessable_entity
     end
@@ -36,6 +54,8 @@ class QuotesController < OpenReadController
   # DELETE /quotes/1
   def destroy
     @quote.destroy
+    # trigger front end build with new data
+    post_netlify_webhook if Rails.env.production?
   end
 
   private
